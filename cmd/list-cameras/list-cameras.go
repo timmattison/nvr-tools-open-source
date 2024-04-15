91a3f40cc8007059a151b68064f44d26971c064e
Tim Mattison <tim@mattison.org>
2024-04-15 16:55:38 -0400

    Basic .env file support

--- cmd/list-cameras/list-cameras.go
+++ cmd/list-cameras/list-cameras.go
@@ -7,19 +7,29 @@ import (
 	"fmt"
 	"github.com/charmbracelet/log"
 	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
 	"github.com/timmattison/nvr-tools-open-source/pkg/nvr-unifi-protect"
+	"os"
 )
 
 func main() {
 	var unifiProtectHost string
 
-	flag.StringVar(&unifiProtectHost, "h", "", "The UniFi Protect host to connect to (IP address or hostname)")
+	flag.StringVar(&unifiProtectHost, "h", "", "The UniFi Protect host to connect to (IP address or hostname) (can also be set via UNIFI_PROTECT_HOST environment variable)")
 
 	flag.Parse()
 
 	if unifiProtectHost == "" {
-		flag.Usage()
-		return
+		if err := godotenv.Load(".env"); err != nil {
+			log.Warn("No .env file found and no host specified")
+		} else {
+			unifiProtectHost = os.Getenv("UNIFI_PROTECT_HOST")
+		}
+
+		if unifiProtectHost == "" {
+			flag.Usage()
+			return
+		}
 	}
 
 	ctx, cancelFunc := context.WithCancelCause(context.Background())
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.2
 require (
 	github.com/charmbracelet/log v0.4.0
 	github.com/jmoiron/sqlx v1.3.5
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.22.0
 )
